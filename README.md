14e15b2741742d5aea54b60fdf095714a0bfbb66
hongsea <hongsea119@gmail.com>
2020-10-06 15:29:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wi-Fi Hotspot Installation
---
+++ install.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+##This script run for install hotspot
+
+##Color
+RED='\033[0;31m'
+GREEN='\e[32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;32m'
+NC='\033[0m'
+
+##banner
+banner(){
+    echo
+    BANNER_NAME=$1
+    echo -e "${YELLOW}[+] ${BANNER_NAME}"
+    echo -e "--------------------------------------${NC}"
+}
+
+##run as root
+check_root(){
+    if [[ $(id -u) != 0 ]]; then
+        echo "This script run as root."
+        exit;
+    fi
+}
+
+##Install package
+package_install(){
+banner "Install Package"
+    for PKG in $(cat $(pwd)/package_x86_64)
+    do
+        dpkg -s ${PKG} $> /dev/null
+        if [ $? -eq 0 ]; then
+            echo -e "${GREEN}[ OK ] Package ${PKG} is installed!${NC}"
+        else
+            echo -e "${GREEN}[ Check ] Installing ${PKG}...!${NC}"
+            apt-get install -y ${PKG}
+            echo -e "${GREEN}[ OK ] Package ${PKG} is installed!${NC}"
+        fi
+    done
+}
+
+##static ip address
+static_ip(){
+banner "Configure static ip address"
+    WIRE=$(ls /sys/class/net/ | grep eth)
+    WIRELESS=$(ls /sys/class/net/ | grep wl)
+
+    echo -e "You are have interfaces wan and lan!"
+    #input wan ip
+    echo "[${WIRE}]: wan interface"
+    read -p "IP Address[]: " WANIP
+    read -p "Netmask[]:" WANNETMASK
+    read -p "Gateway[]:" WANGATEWAY
+
+
+    #input lan ip
+    echo -e "\n[${WIRELESS}]: lan interface"
+    read -p "IP Address[]:" LANIP
+    read -p "Netmask[]:" LANNETMASK
+    read -p "Gateway[]:" LANGATEWAY    
+
+    #copy config to /etc/network
+    cp $(pwd)/network /etc/
+
+    #replace name in config file
+    #wan interface
+    grep -rli WANIP /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANIP+${WANIP}+g @
+    grep -rli WANNETMASK /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANNETMASK+${WANNETMASK}+g @
+    grep -rli WANGATEWAY /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANGATEWAY+${WANGATEWAY}+g @
+    echo -e "${GREEN}[ OK ] Configure wan interface!${NC}"
+
+    #lan interface
+    grep -rli LANIP /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANIP+${LANIP}+g @
+    grep -rli LANNETMASK /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANNETMASK+${LANNETMASK}+g @
+    grep -rli LANGATEWAY /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANGATEWAY+${LANGATEWAY}+g @    
+    echo -e "${GREEN}[ OK ] Configure lan interface!${NC}"
+
+    #start service
+    systemctl restart networking
+}
+
+##dhcp
+dhcp(){
+banner "Configure dhcp"
+
+    PREFIX1=$(echo ${LANIP} | awk -F'.' '{print $1}')
+    PREFIX2=$(echo ${LANIP} | awk -F'.' '{print $2}')
+    PREFIX3=$(echo ${LANIP} | awk -F'.' '{print $3}')
+
+    NETWORK="${PREFIX1}.${PREFIX2}.${PREFIX3}.0"
+    SUBNET="${LANNETMASK}"
+    GATEWAY="${LANGATEWAY}"
+    RANK="${PREFIX1}.${PREFIX2}.${PREFIX3}.10 ${PREFIX1}.${PREFIX2}.${PREFIX3}.254"
+
+    echo "Network[]: ${NETWORK}"
+    echo "Subnet[]: ${SUBNET}"
+    echo "Gateway[]: ${GATEWAY}"
+    read -p "We recommended use rank (10-254) [Y\n]: " YN
+
+    cp $(pwd)/dhcp/dhcpd.conf /etc/dhcp/
+
+}
+
+##call function
+check_root
+package_install
+static_ip
