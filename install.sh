346a2a92bfe0e1bf05e73d593701bc909303d532
hongsea <hongsea119@gmail.com>
2020-11-17 13:31:03 +0700

    add bind & update function

--- install.sh
+++ install.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-##This script run for install hotspot
-
-aonther(){
-    rfkill unblock all
-    apt-get update
-}
-
-##Color
-RED='\033[0;31m'
-GREEN='\e[32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;32m'
-NC='\033[0m'
-
-WIRE=$(ls /sys/class/net/ | grep eth | awk 'NR==1')
-WIRELESS=$(ls /sys/class/net/ | grep wl | awk 'NR==1')
-
-##banner
-banner(){
-    echo
-    BANNER_NAME=$1
-    echo -e "${YELLOW}[+] ${BANNER_NAME}"
-    echo -e "--------------------------------------${NC}"
-}
+##This script run for install Wi-Fi hotspot.
+##It management by Hongsea Heng.
 
 ##run as root
 check_root(){
@@ -32,228 +10,83 @@ check_root(){
         exit;
     fi
 }
+check_root
 
-##Install package
-package_install(){
-banner "Install Package"
+#check wire and wireless adapter
+WIRE=$(ls /sys/class/net/ | grep eth | awk 'NR==1')
+WIRELESS=$(ls /sys/class/net/ | grep wl | awk 'NR==1')
 
-    for PKG in $(cat $(pwd)/package_x86_64)
-    do
-        dpkg -s ${PKG} &> /dev/null
-        if [ $? -eq 0 ]; then
-            echo -e "${GREEN}[ OK ] Package ${PKG} is installed!${NC}"
-        else
-            echo -e "${GREEN}[ Check ] Installing ${PKG}...!${NC}"
-            apt-get install -y ${PKG}
-            echo -e "${GREEN}[ OK ] Package ${PKG} is installed!${NC}"
-        fi
-    done
+##static-ip-config.sh input
+echo -e "You are have interfaces wan and lan!"
+read -p "Do you config wan interface as dhcp client[Y/n]: " YN
 
-    sudo dpkg -i $(pwd)/package/coova-chilli_1.4_armhf.deb
-}
-
-##default
-default_config(){
-banner "Configure Default"
-
-    cp -r $(pwd)/default /etc
-    echo -e "${GREEN}[ OK ] Configure Default config!${NC}"
-}
-
-##static ip address
-static_ip_config(){
-banner "Configure static ip address"
-
-    echo -e "You are have interfaces wan and lan!"
-    #input wan ip
-    echo "[${WIRE}]: is WAN interface"
+if [[ ${YN} == y || ${YN} == Y || -z ${YN} ]]; then
+    echo "You are choose wan interface as dhcp client...!"
+else
+    echo "You are choose wan interface as static ip address...!"
+    echo "Please input wlan IP"
+    echo "[${WIRE}]: is WAN Interface"
     read -p "IP Address[]: " WANIP
     read -p "Netmask[]: " WANNETMASK
-    read -p "Gateway[]: " WANGATEWAY
+    read -p "Gateway[]: " WANGATEWAY    
+fi
 
+echo "Lan interface need to static ip address"
+echo "Please input lan IP"
+#input lan ip
+echo -e "\n[${WIRELESS}]: is LAN Interface"
+read -p "IP Address[]: " LANIP
+read -p "Netmask[]: " LANNETMASK
+read -p "Gateway[]: " LANGATEWAY 
 
-    #input lan ip
-    echo -e "\n[${WIRELESS}]: is LAN interface"
-    read -p "IP Address[]: " LANIP
-    read -p "Netmask[]: " LANNETMASK
-    read -p "Gateway[]: " LANGATEWAY    
+##postgresql-config.sh input
+read -p "Postgresql Username[]: " SQLUSERNAME
+read -p "Postgresql Password[]: " SQLPASSWORD
 
-    #copy config to /etc/network
-    cp -r $(pwd)/staticIP/network /etc/
+##hostapd-config.sh input
+read -p "SSID Name[]: " SSID
 
-    #replace name in config file
-    #wan interface
-    grep -rli WANIP /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANIP+${WANIP}+g @
-    grep -rli WANNETMASK /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANNETMASK+${WANNETMASK}+g @
-    grep -rli WANGATEWAY /etc/network/interfaces.d/eth0-wan | xargs -i@ sed -i s+WANGATEWAY+${WANGATEWAY}+g @
-    echo -e "${GREEN}[ OK ] Configure wan interface!${NC}"
+##freeradius-config.sh input
+echo "Connection info sql"
+read -p "Server[]: " IPSERVERRADIUS
+read -p "Port[]: " PORT
+read -p "Radius password[]: " RADIUSPASSWD
 
-    #lan interface
-    grep -rli LANIP /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANIP+${LANIP}+g @
-    grep -rli LANNETMASK /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANNETMASK+${LANNETMASK}+g @
-    grep -rli LANGATEWAY /etc/network/interfaces.d/wlan0-lan | xargs -i@ sed -i s+LANGATEWAY+${LANGATEWAY}+g @    
-    echo -e "${GREEN}[ OK ] Configure lan interface!${NC}"
+##bond-config.sh input
+echo "Hotspot Domain"
+read -p "Domain[Example: domain.com]: " DOMAIN
 
-    #start service
-    ifconfig wlan0 up
-    ifconfig eth0 up
-    systemctl restart networking
+##Package Install file
+$(pwd)/src/package-install.sh
+
+##Configure Default file
+$(pwd)/src/default-config.sh
+
+##Configure ip adress
+static-config(){
+    $(pwd)/src/ip-config.sh "$WIRE" "$WIFINAME" "$YN" "$YN" "$WANIP" "$WANNETMASK" "$WANGATEWAY" "$LANIP" "$LANNETMASK" "$LANGATEWAY"
 }
 
-##dhcp
-dhcp_config(){
-banner "Configure dhcp"
-
-    PREFIX1=$(echo ${LANIP} | awk -F'.' '{print $1}')
-    PREFIX2=$(echo ${LANIP} | awk -F'.' '{print $2}')
-    PREFIX3=$(echo ${LANIP} | awk -F'.' '{print $3}')
-
-    NETWORK="${PREFIX1}.${PREFIX2}.${PREFIX3}.0"
-    SUBNET="${LANNETMASK}"
-    GATEWAY="${LANGATEWAY}"
-
-    echo "Network[]: ${NETWORK}"
-    echo "Subnet[]: ${SUBNET}"
-    echo "Gateway[]: ${GATEWAY}"
-
-    read -p "We recommended use range (10-254) [Y\n]: " YN
-    if [[ "${YN}" == "Y" || "${YN}" == "y" || -z "${YN}" ]];then
-        RANK="${PREFIX1}.${PREFIX2}.${PREFIX3}.10 ${PREFIX1}.${PREFIX2}.${PREFIX3}.254"
-        echo "Rank[]: ${RANK}"
-    else
-        read -p "Rank start from[]: " START
-        read -p "To[]: " END
-        RANK="${PREFIX1}.${PREFIX2}.${PREFIX3}.${START} ${PREFIX1}.${PREFIX2}.${PREFIX3}.${END}"
-        echo "Rank[]: ${RANK}"
-    fi
-
-    cp $(pwd)/dhcp/dhcpd.conf /etc/dhcp/
-    echo -e "${GREEN}[ OK ] Copy dhcp config${NC}"
-
-    grep -rli IPNETWORK /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPNETWORK+${NETWORK}+g @
-    grep -rli IPSUBNET /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPSUBNET+${SUBNET}+g @
-    grep -rli IPRANK /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPRANK+"${RANK}"+g @
-    grep -rli IPGATEWAY /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPGATEWAY+${GATEWAY}+g @
-    grep -rli LANINTERFACE /etc/default/isc-dhcp-server | xargs -i@ sed -i s+LANINTERFACE+${WIRELESS}+g @
-    echo -e "${GREEN}[ OK ] Configure dhcp${NC}"
-
-    systemctl disable dhcpcd
-    systemctl enable isc-dhcp-server
-    systemctl start isc-dhcp-server
-    echo -e "${GREEN}[ OK ] Start service dhcp!${NC}"
+dhcp-config(){
+    $(pwd)/src/ip-config.sh "$WIRE" "$WIFINAME" "$YN" "$LANIP" "$LANNETMASK" "$LANGATEWAY"
 }
+if [[ $YN == y || $YN == Y || -z $YN ]]; then
+    dhcp-config
+else
+    static-config
+fi
 
-##postgresql
-postgresql_config(){
-banner "Install Postgresql"
+##Configure postgresql file
+$(pwd)/src/postgresql-config.sh "$SQLUSERNAME" "$SQLPASSWORD"
 
-    systemctl enable docker
-    sudo systemctl start docker
-    echo -e "${GREEN}[ OK ] Start service docker!${NC}"
+##Configure hostapd file
+$(pwd)/src/hostapd-config.sh "$SSID" "$WIRELESS"
 
-    read -p "Postgresql Username[]: " USERNAME
-    read -p "Postgresql Password[]: " PASSWORD
+##Configure freeradius file
+$(pwd)/src/freeradius-config.sh "$IPSERVERRADIUS" "$PORT" "$SQLUSERNAME" "$SQLPASSWORD" "$RADIUSPASSWD"
 
-    cp -r postgresql /opt/
-    echo -e "${GREEN}[ OK ] Copy docker compose!${NC}"
+##Configure chilli file
+$(pwd)/src/coovachilli-config.sh "$WIRE" "$WIRELESS" "$LANIP" "$LANNETMASK" "$LANGATEWAY" "$SSID" "$RADIUSPASSWD"
 
-    grep -rli USERNAME /opt/postgresql/docker-compose.yml | xargs -i@ sed -i s+USERNAME+"${USERNAME}"+g @
-    grep -rli PASSWD /opt/postgresql/docker-compose.yml | xargs -i@ sed -i s+PASSWD+${PASSWORD}+g @
-    echo -e "${GREEN}[ OK ] Configure docker-compose!${NC}"
-
-    docker-compose -f /opt/postgresql/docker-compose.yml down
-    docker-compose -f /opt/postgresql/docker-compose.yml up -d
-    echo -e "${GREEN}[ OK ] Start docker-compose!${NC}"
-
-}
-
-##hostapd
-hostapd_config(){
-banner "Configure hostapd"
-
-    read -p "SSID Name[]: " SSID
-    cp -r hostapd /etc
-    echo -e "${GREEN}[ OK ] Copy config!${NC}"
-
-    grep -rli LANINTERFACE /etc/hostapd/hostapd.conf | xargs -i@ sed -i s+LANINTERFACE+${WIRELESS}+g @
-    grep -rli WIFINAME /etc/hostapd/hostapd.conf | xargs -i@ sed -i s+WIFINAME+${SSID}+g @
-    echo -e "${GREEN}[ OK ] Configure hostapd!${NC}"
-
-    systemctl enable hostapd
-    systemctl start hostapd
-    echo -e "${GREEN}[ OK ] Start service hostapd!${NC}"
-    
-}
-
-##freeradius
-freeradius_config(){
-banner "Configure freeradius"
-
-    #sql Connection info
-    echo "Connection info sql"
-    read -p "Server[]: " IPSERVER
-    read -p "Port[]: " PORT
-    read -p "Radius password[]: " RADIUSPASSWD
-
-    cp -r $(pwd)/freeradius /etc/
-    echo -e "${GREEN}[ OK ] Copy config.${NC}"
-
-    grep -rli IPSERVER /etc/freeradius/3.0/mods-available/sql | xargs -i@ sed -i s+IPSERVER+${IPSERVER}+g @
-    grep -rli PORTNUMBER /etc/freeradius/3.0/mods-available/sql | xargs -i@ sed -i s+PORTNUMBER+${PORT}+g @
-    grep -rli USERNAME /etc/freeradius/3.0/mods-available/sql | xargs -i@ sed -i s+USERNAME+${USERNAME}+g @
-    grep -rli PASSWORDLOGIN /etc/freeradius/3.0/mods-available/sql | xargs -i@ sed -i s+PASSWORDLOGIN+${PASSWORD}+g @
-    echo -e "${GREEN}[ OK ] Configure freeradius sql!${NC}"
-
-    ln -s /etc/freeradius/3.0/sites-avaiable/default /etc/freeradius/3.0/sites-enabled/default
-    ln -s /etc/freeradius/3.0/sites-available/inner-tunner /etc/freeradius/3.0/sites-enabled/inner-tunner
-    ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/sql
-    echo -e "${GREEN}[ OK ] Link configure freeradius!${NC}"
-
-    #secret
-    grep -rli RADIUSSECRET /etc/freeradius/3.0/client.conf | xargs -i@ sed -i s+RADIUSSECRET+${RADIUSPASSWD}+g @
-
-    systemctl enable freeradius
-    systemctl start freeradius
-    echo -e "${GREEN}[ OK ] Start service freeradius!${NC}"
-
-}
-
-##coovachilli
-coovachilli_config(){
-banner "Configure coovachilli"
-
-    cp -r $(pwd)/coovachilli/* /
-    echo -e "${GREEN}[ OK ] Copy config!${NC}"
-
-    grep -rli INTERFACEWAN /etc/chilli/config | xargs -i@ sed -i s+INTERFACEWAN+${WIRE}+g @
-    grep -rli INTERFACELAN /etc/chilli/config | xargs -i@ sed -i s+INTERFACELAN+${WIRELESS}+g @
-    grep -rli IPNETWORK /etc/chilli/config | xargs -i@ sed -i s+IPNETWORK+${NETWORK}+g @
-    grep -rli IPSUBNET /etc/chilli/config | xargs -i@ sed -i s+IPSUBNET+${SUBNET}+g @
-    grep -rli IPGATEWAY /etc/chilli/config | xargs -i@ sed -i s+IPGATEWAY+${GATEWAY}+g @
-    grep -rli SSIDNAME /etc/chilli/config | xargs -i@ sed -i s+SSIDNAME+${SSID}+g @
-    grep -rli PASSRAD /etc/chilli/config | xargs -i@ sed -i s+PASSRAD+${RADIUSPASSWD}+g @
-    echo -e "${GREEN}[ OK ] Configure coova!${NC}"
-
-    systemctl enable chilli
-    systemctl start chilli
-    echo -e "${GREEN}[ OK ] Start service chilli"
-
-}
-
-##Service status
-# server_status(){
-#     systemctl status dhcpd.service hostapd.service freeradius.service docker.service 
-# }
-
-##call function
-check_root
-aonther
-package_install
-default_config
-static_ip_config
-dhcp_config
-postgresql_config
-hostapd_config
-freeradius_config
-coovachilli_config
-# service_status
+##Configure bind file
+$(pwd)/src/bind-config.sh "$DOMAIN" "$LANIP"
---
+++ src/dhcp-config.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+##color
+RED='\033[0;31m'
+GREEN='\e[32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;32m'
+NC='\033[0m' 
+
+##get arguments from install.sh
+LANIP=$1
+LANNETMASK=$2
+LANGATEWAY=$3
+
+##dhcp
+dhcp_config(){
+./src/banner.sh "Configure dhcp"
+
+    PREFIX1=$(echo ${LANIP} | awk -F'.' '{print $1}')
+    PREFIX2=$(echo ${LANIP} | awk -F'.' '{print $2}')
+    PREFIX3=$(echo ${LANIP} | awk -F'.' '{print $3}')
+
+    NETWORK="${PREFIX1}.${PREFIX2}.${PREFIX3}.0"
+    SUBNET="${LANNETMASK}"
+    GATEWAY="${LANGATEWAY}"
+
+    echo "Network[]: ${NETWORK}"
+    echo "Subnet[]: ${SUBNET}"
+    echo "Gateway[]: ${GATEWAY}"
+
+    read -p "We recommended use range (10-254) [Y\n]: " YN
+    if [[ "${YN}" == "Y" || "${YN}" == "y" || -z "${YN}" ]];then
+        RANK="${PREFIX1}.${PREFIX2}.${PREFIX3}.10 ${PREFIX1}.${PREFIX2}.${PREFIX3}.254"
+        echo "Rank[]: ${RANK}"
+    else
+        read -p "Rank start from[]: " START
+        read -p "To[]: " END
+        RANK="${PREFIX1}.${PREFIX2}.${PREFIX3}.${START} ${PREFIX1}.${PREFIX2}.${PREFIX3}.${END}"
+        echo "Rank[]: ${RANK}"
+    fi
+
+    cp -v $(pwd)/dhcp/dhcpd.conf /etc/dhcp/
+    grep -rli IPNETWORK /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPNETWORK+${NETWORK}+g @
+    grep -rli IPSUBNET /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPSUBNET+${SUBNET}+g @
+    grep -rli IPRANK /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPRANK+"${RANK}"+g @
+    grep -rli IPGATEWAY /etc/dhcp/dhcpd.conf | xargs -i@ sed -i s+IPGATEWAY+${GATEWAY}+g @
+    grep -rli LANINTERFACE /etc/default/isc-dhcp-server | xargs -i@ sed -i s+LANINTERFACE+${WIRELESS}+g @
+    if [[ "$?" == 0 ]]; then
+        echo -e "${GREEN}[ OK ] Configure dhcp${NC}"
+    else
+        echo -e "${RED}[ Failed ] Configure dhcp${NC}"
+    fi
+
+    systemctl disable dhcpcd
+    systemctl enable isc-dhcp-server
+    systemctl start isc-dhcp-server
+    echo -e "${GREEN}[ OK ] Start service dhcp!${NC}"
+}
+
+dhcp_config
