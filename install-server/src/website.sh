04e6c0ef11829c705677d934d23dcd49d9161753
hongsea <hongsea119@gmail.com>
2021-03-30 19:18:08 +0700

    add pm2

--- install-server/src/website.sh
+++ install-server/src/website.sh
@@ -10,11 +10,8 @@ NC='\033[0m'
 function website(){
 ./src/banner.sh "Running website"
 
-    sudo cp -r ./local-content /var/www/
+    sudo cp -r ./local-content/* /var/www/
     echo -e "${GREEN}[ OK ] Copy source code.${NC}"
-
-    npm install pm2
-    echo -e "${GREEN}[ OK ] Install pm2.${NC}"
 
     echo -e "${GREEN}[ OK ] Pm2 on startup.${NC}"
     GET=$(pm2 startup | grep sudo)
@@ -22,6 +19,9 @@ function website(){
     
     echo -e "${GREEN}[ RUN ] Website: world.koompi.org${NC}"
     cd /var/www/world.koompi.org/
+    npm install pm2
+    echo -e "${GREEN}[ OK ] Install pm2.${NC}"
+    
     npm install
     pm2 --name world.koompi.org start npm index.js
     pm2 save
